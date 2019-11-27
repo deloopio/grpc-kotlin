293234f9f4af52d41614945d0dae52c638e5205a
Plato Balomenakis <plato.blm@gmail.com>
2019-11-27 12:34:55 +0000

    Format files. Rename Proxy.kt generated file

--- grpc-kotlin-gen/src/main/java/io/rouz/grpc/kotlin/GrpcKotlinGenerator.java
+++ grpc-kotlin-gen/src/main/java/io/rouz/grpc/kotlin/GrpcKotlinGenerator.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -52,7 +52,6 @@ public class GrpcKotlinGenerator extends Generator {
 
   private static final int SERVICE_NUMBER_OF_PATHS = 2;
   private static final int METHOD_NUMBER_OF_PATHS = 4;
-  private static final String CLASS_SUFFIX = "ImplBase";
   private static final String STUB_SUFFIX = "StubExt";
   private static final String ADAPTERS_FILE_PATH = "io/rouz/grpc/Adapters.kt";
   private static final String SERVICE_JAVA_DOC_PREFIX = "    ";
@@ -64,12 +63,12 @@ public class GrpcKotlinGenerator extends Generator {
 
   @Override
   public List<CodeGeneratorResponse.File> generateFiles(CodeGeneratorRequest request)
-      throws GeneratorException {
+    throws GeneratorException {
     final ProtoTypeMap typeMap = ProtoTypeMap.of(request.getProtoFileList());
 
     List<FileDescriptorProto> protosToGenerate = request.getProtoFileList().stream()
-        .filter(protoFile -> request.getFileToGenerateList().contains(protoFile.getName()))
-        .collect(Collectors.toList());
+      .filter(protoFile -> request.getFileToGenerateList().contains(protoFile.getName()))
+      .collect(Collectors.toList());
 
     List<Context> services = findServices(protosToGenerate, typeMap);
     return generateFiles(services);
@@ -81,17 +80,17 @@ public class GrpcKotlinGenerator extends Generator {
     protos.forEach(fileProto -> {
       List<Location> locations = fileProto.getSourceCodeInfo().getLocationList();
       locations.stream()
-          .filter(location -> location.getPathCount() == SERVICE_NUMBER_OF_PATHS
-              && location.getPath(0) == FileDescriptorProto.SERVICE_FIELD_NUMBER)
-          .forEach(location -> {
-            int serviceNumber = location.getPath(SERVICE_NUMBER_OF_PATHS - 1);
-            Context context = context(
-                fileProto.getService(serviceNumber), typeMap, locations, serviceNumber);
-            context.javaDoc = getJavaDoc(getComments(location), SERVICE_JAVA_DOC_PREFIX);
-            context.protoName = fileProto.getName();
-            context.packageName = extractPackageName(fileProto);
-            contexts.add(context);
-          });
+        .filter(location -> location.getPathCount() == SERVICE_NUMBER_OF_PATHS
+          && location.getPath(0) == FileDescriptorProto.SERVICE_FIELD_NUMBER)
+        .forEach(location -> {
+          int serviceNumber = location.getPath(SERVICE_NUMBER_OF_PATHS - 1);
+          Context context = context(
+            fileProto.getService(serviceNumber), typeMap, locations, serviceNumber);
+          context.javaDoc = getJavaDoc(getComments(location), SERVICE_JAVA_DOC_PREFIX);
+          context.protoName = fileProto.getName();
+          context.packageName = extractPackageName(fileProto);
+          contexts.add(context);
+        });
     });
 
     return contexts;
@@ -110,29 +109,29 @@ public class GrpcKotlinGenerator extends Generator {
   }
 
   private Context context(
-      ServiceDescriptorProto serviceProto,
-      ProtoTypeMap protoTypeMap,
-      List<Location> locations,
-      int serviceNumber) {
+    ServiceDescriptorProto serviceProto,
+    ProtoTypeMap protoTypeMap,
+    List<Location> locations,
+    int serviceNumber) {
 
     Context context = new Context();
     context.serviceName = serviceProto.getName();
     context.deprecated = serviceProto.getOptions() != null
-        && serviceProto.getOptions().getDeprecated();
+      && serviceProto.getOptions().getDeprecated();
 
     locations.stream()
-        .filter(location -> location.getPathCount() == METHOD_NUMBER_OF_PATHS
-                            && location.getPath(0) == FileDescriptorProto.SERVICE_FIELD_NUMBER
-                            && location.getPath(1) == serviceNumber
-                            && location.getPath(2) == ServiceDescriptorProto.METHOD_FIELD_NUMBER)
-        .forEach(location -> {
-          int methodNumber = location.getPath(METHOD_NUMBER_OF_PATHS - 1);
-          MethodContext methodContext = methodContext(
-              serviceProto.getMethod(methodNumber), protoTypeMap);
-          methodContext.methodNumber = methodNumber;
-          methodContext.javaDoc = getJavaDoc(getComments(location), METHOD_JAVA_DOC_PREFIX);
-          context.methods.add(methodContext);
-        });
+      .filter(location -> location.getPathCount() == METHOD_NUMBER_OF_PATHS
+        && location.getPath(0) == FileDescriptorProto.SERVICE_FIELD_NUMBER
+        && location.getPath(1) == serviceNumber
+        && location.getPath(2) == ServiceDescriptorProto.METHOD_FIELD_NUMBER)
+      .forEach(location -> {
+        int methodNumber = location.getPath(METHOD_NUMBER_OF_PATHS - 1);
+        MethodContext methodContext = methodContext(
+          serviceProto.getMethod(methodNumber), protoTypeMap);
+        methodContext.methodNumber = methodNumber;
+        methodContext.javaDoc = getJavaDoc(getComments(location), METHOD_JAVA_DOC_PREFIX);
+        context.methods.add(methodContext);
+      });
     return context;
   }
 
@@ -142,7 +141,7 @@ public class GrpcKotlinGenerator extends Generator {
     methodContext.inputType = typeMap.toJavaTypeName(methodProto.getInputType());
     methodContext.outputType = typeMap.toJavaTypeName(methodProto.getOutputType());
     methodContext.deprecated = methodProto.getOptions() != null
-        && methodProto.getOptions().getDeprecated();
+      && methodProto.getOptions().getDeprecated();
     methodContext.isManyInput = methodProto.getClientStreaming();
     methodContext.isManyOutput = methodProto.getServerStreaming();
     if (!methodProto.getClientStreaming() && !methodProto.getServerStreaming()) {
@@ -181,30 +180,30 @@ public class GrpcKotlinGenerator extends Generator {
     String content = applyTemplate("Adapters.mustache", utilContext);
 
     return PluginProtos.CodeGeneratorResponse.File
-        .newBuilder()
-        .setName(ADAPTERS_FILE_PATH)
-        .setContent(content)
-        .build();
+      .newBuilder()
+      .setName(ADAPTERS_FILE_PATH)
+      .setContent(content)
+      .build();
   }
 
   private PluginProtos.CodeGeneratorResponse.File buildServiceBaseImpl(Context context) {
-    String content = applyTemplate("ImplBase.mustache", context);
-    String fileName = context.serviceName + CLASS_SUFFIX + ".kt";
+    String content = applyTemplate("ProxyImpl.mustache", context);
+    String fileName = context.serviceName + "Proxy.kt";
     return PluginProtos.CodeGeneratorResponse.File
-        .newBuilder()
-        .setName(absoluteFileName(context.packageName, fileName))
-        .setContent(content)
-        .build();
+      .newBuilder()
+      .setName(absoluteFileName(context.packageName, fileName))
+      .setContent(content)
+      .build();
   }
 
   private PluginProtos.CodeGeneratorResponse.File buildStubExtensions(Context context) {
     String content = applyTemplate("StubExtensions.mustache", context);
     String fileName = context.serviceName + STUB_SUFFIX + ".kt";
     return PluginProtos.CodeGeneratorResponse.File
-        .newBuilder()
-        .setName(absoluteFileName(context.packageName, fileName))
-        .setContent(content)
-        .build();
+      .newBuilder()
+      .setName(absoluteFileName(context.packageName, fileName))
+      .setContent(content)
+      .build();
   }
 
   private String absoluteFileName(String packageName, String fileName) {
@@ -218,23 +217,23 @@ public class GrpcKotlinGenerator extends Generator {
 
   private String getComments(Location location) {
     return location.getLeadingComments().isEmpty()
-        ? location.getTrailingComments()
-        : location.getLeadingComments();
+      ? location.getTrailingComments()
+      : location.getLeadingComments();
   }
 
   private String getJavaDoc(String comments, String prefix) {
     if (!comments.isEmpty()) {
       StringBuilder builder = new StringBuilder("/**\n")
-          .append(prefix).append(" * <pre>\n");
+        .append(prefix).append(" * <pre>\n");
       Arrays.stream(HtmlEscapers.htmlEscaper().escape(comments).split("\n"))
-          // Kotlin allows nested block comments, so any occurrence of '/*' would begin
-          // a nested block, breaking the generated code.
-          .map(line -> line.replace("/*", "/&#42;"))
-          .map(line -> line.replace("*/", "*&#47;"))
-          .forEach(line -> builder.append(prefix).append(" * ").append(line).append("\n"));
+        // Kotlin allows nested block comments, so any occurrence of '/*' would begin
+        // a nested block, breaking the generated code.
+        .map(line -> line.replace("/*", "/&#42;"))
+        .map(line -> line.replace("*/", "*&#47;"))
+        .forEach(line -> builder.append(prefix).append(" * ").append(line).append("\n"));
       builder
-          .append(prefix).append(" * <pre>\n")
-          .append(prefix).append(" */");
+        .append(prefix).append(" * <pre>\n")
+        .append(prefix).append(" */");
       return builder.toString();
     }
     return null;
@@ -276,8 +275,8 @@ public class GrpcKotlinGenerator extends Generator {
         char c = methodName.charAt(i);
         s.append(Character.toUpperCase(c));
         if ((i < methodName.length() - 1)
-            && Character.isLowerCase(c)
-            && Character.isUpperCase(methodName.charAt(i + 1))) {
+          && Character.isLowerCase(c)
+          && Character.isUpperCase(methodName.charAt(i + 1))) {
           s.append('_');
         }
       }
